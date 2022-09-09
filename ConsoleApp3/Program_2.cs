68ac056d01a468a385c7ad30360a98014cd3afb7
Ivan <ianoxx@outlook.com>
2022-09-09 14:36:51 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program_2.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fdsfsd
+{
+    class Program
+
+    {
+
+
+        static double fy2(int i, double xt, double[] x, double[] y)
+        {
+            double A, B, C;
+            A = y[i - 1];
+            B = (y[i] - A) / (x[i] - x[i - 1]);
+            C = (y[i + 1] - A - B * (x[i + 1] - x[i - 1])) / ((x[i + 1] - x[i - 1]) * (x[i + 1] - x[i]));
+            return A + B * (xt - x[i - 1]) + C * (xt - x[i]) * (xt - x[i - 1]);
+        }
+        static double fy3(int i, double xt, double[] x, double[] y)
+        {
+            //Кубическая интерполяция
+            double A, B, C, D;
+            A = y[i - 1];
+            B = (y[i] - A) / (x[i] - x[i - 1]);
+            C = (B - ((y[i - 1] - A) / x[i - 1])) / (x[i] - x[i - 1]);
+            D = (((y[i] + A) / x[i]) - B - C * (x[i] - x[i - 1])) / ((x[i] - x[i - 1]) * (x[i] - x[i - 1]));
+            return A + B * (xt - x[i - 1]) + C * (xt - x[i]) * (xt - x[i - 1]) + D * (xt - x[i - 1]) * (xt - x[i - 1]) * (xt - x[i]);
+
+        }
+
+
+        static void Main()
+        {
+            double g;
+            double yt = 0;
+            double[] x = new double[] { 927, 1077, 1227, 1377, 1527, 1677, 1827, 1977, 2120, 2125, 2127 };
+            double[] y = new double[] { 75, 69, 63, 41, 20, 14, 8, 6, 4.2, 4, 4 };
+            for (int i = 0; i < x.Length; i++)
+            {
+                Console.Write("x = {0,3}      y={1,2} ", x[i], y[i]);
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Введите значение х ");
+                g = double.Parse(Console.ReadLine());
+                double xt = g;
+                Console.WriteLine();
+                if (xt <= ((x[1] + x[2]) / 2))
+                {
+                    yt = fy2(2, xt, x, y);
+                }
+                else
+                {
+                    for (int j = 2; j < 10; j++)
+                    {
+                        if ((xt >= (x[j - 1] + x[j]) / 2) & xt <= ((x[j] + x[j + 1]) / 2))
+                        {
+                            yt = fy2(j, xt, x, y);
+                            break;
+                        }
+                    }
+                }
+                Console.WriteLine("Квадратичная интерполяция");
+                Console.Write("В точке x={0:f1}", xt);
+                Console.WriteLine("  значение y={0:f1}", yt);
+                Console.WriteLine();
+                if (xt <= ((x[1] + x[2]) / 2))
+                {
+                    yt = fy3(2, xt, x, y);
+                }
+                else
+                {
+                    for (int j = 2; j < 10; j++)
+                    {
+                        if ((xt >= (x[j - 1] + x[j]) / 2) & xt <= ((x[j] + x[j + 1]) / 2))
+                        {
+                            yt = fy3(j, xt, x, y);
+                            break;
+                        }
+                    }
+                }
+                Console.WriteLine("Кубическая интерполяция");
+                Console.Write("В точке x={0:f1}", xt);
+                Console.WriteLine("  значение y={0:f1}", yt);
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+
+}
---
+++ ConsoleApp3/Program_4.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            int xc = this.Width / 2;
+            int yc = this.Height / 2;
+            g.TranslateTransform(xc, yc);
+            g.DrawEllipse(new Pen(Color.Brown, 0.1f), 0, 0, 0, 0);
+            double x;
+            float z;
+            g.DrawLine(new Pen(Color.Black, 3.0f), -500, 0, 500, 0);
+            g.DrawLine(new Pen(Color.Black, 3.0f), 0, -500, 0, 500);
+            for (x = -5; x <= 5; x++)
+            {
+                float zx = (float)x;
+                g.DrawLine(new Pen(Color.Black, 1.0f), zx * 100, -5, zx * 100, 5);
+
+                float x1 = (float)x;
+                g.DrawLine(new Pen(Color.Black, 1.0f), -5, -x1 * 100, 5, -x1 * 100);
+            }
+            double X1 = 1, X2 = 3.0, dx = (X2 - X1) / 40.0;
+            for (x = X1; x <= X2; x += dx)
+            {
+                z = (float)(3 - Math.Log(x) * Math.Log(x) + 6 * Math.Log(x) - 5);
+                float zx = (float)x;
+                g.DrawEllipse(new Pen(Color.Red, 4.0f), zx * 100, -z * 100, 1.0f, 1.0f);
+
+                Font font = new Font("Arial", 14);
+                SolidBrush brush = new SolidBrush(Color.Black);
+                string s2 = "y"; float x2 = 5.0F; float y2 = -350.0F;
+                g.DrawString(s2, font, brush, x2, y2);
+                string s3 = "x"; float x3 = 450.0F; float y3 = 0.0F;
+                g.DrawString(s3, font, brush, x3, y3);
+                s3 = "0"; x3 = -20.0F; y3 = 0.0F;
+                g.DrawString(s3, font, brush, x3, y3);
+                s3 = "1"; x3 = 90.0F; y3 = -25.0F;
+                g.DrawString(s3, font, brush, x3, y3);
+                s3 = "1"; x3 = -25.0F; y3 = -110.0F;
+                g.DrawString(s3, font, brush, x3, y3);
+                s3 = "y= 3 – (ln(x))^2 + 6 ln(x)-5 "; x3 = 150.0F; y3 = 200.0F;
+                g.DrawString(s3, font, brush, x3, y3);
+
+
+
+            }
+        }
+    }
+}
+
